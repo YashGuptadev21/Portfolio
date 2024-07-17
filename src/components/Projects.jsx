30de89370ca5943b907d4f925845303fcea1111e
YashGuptadev21 <you@example.com>
2024-07-17 21:40:00 +0530

    Initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,11 @@ const Projects = () => {
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div 
+            whileInView={{ opacity : 1 , x : 0}}
+            initial = {{ opacity : 0, x : -100 }}
+            transition={{duration : 0.5}}
+            className="w-full lg:w-1/4">
               <img
                 src={project.image}
                 width={150}
@@ -21,8 +25,12 @@ const Projects = () => {
                 alt={project.title}
                 className="mb-6 rounded"
               />
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+            whileInView={{ opacity : 1 , x : 0.}}
+            initial = {{ opacity : 0, x : 100}}
+            transition={{duration  : 1}}
+             className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
@@ -33,7 +41,7 @@ const Projects = () => {
                   {tech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
